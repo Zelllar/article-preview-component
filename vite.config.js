f4265f592295123a63ba393a71cbc107d30ca618
Zelllar <stinusik12@gmail.com>
2025-11-25 13:49:49 +0800

    configuring configs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/article-preview-component/',
   plugins: [react()],
   server: {
     allowedHosts: [
